2f6df82d69f0ea28e823bfc7bc8f9a6b3e56c2c7
twinnerhenock <demessiehe@gmail.com>
2023-07-13 17:33:16 +0300

    demo

--- WorkingTelegramChannelMemeberAdder.py
+++ WorkingTelegramChannelMemeberAdder.py
@@ -41,7 +41,7 @@ chats = []
 last_date = None
 chunk_size = 20000
 groups=[]
-channeluser= 'business key' #USERNAME OF THE CHANNEL WHERE THE MEMBERS WILL BE ADDED
+channeluser= 'demo_group' #USERNAME OF THE CHANNEL WHERE THE MEMBERS WILL BE ADDED
 
 result = client(GetDialogsRequest(
              offset_date=last_date,
